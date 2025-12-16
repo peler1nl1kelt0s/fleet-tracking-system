fc81f6d5646196edbd14644f1f3c903e3cec76f1
Yusuf <yusufdgrl72@gmail.com>
2025-12-17 02:28:38 +0300

    fixed(backend): implementing mock datas (for now)

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,25 +2,18 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import http from 'http';
+import app from './app.js';
+import { initSocket } from './ws/socket.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.join(__dirname, '../../.env') });
-import app from './app.js';
-import { initSocket } from './ws/socket.js';
 
 const PORT = process.env.API_PORT || 3000;
 
 const server = http.createServer(app);
-
 initSocket(server);
 
-server.listen(PORT, async () => {
-  console.log(`🚀 Backend running on port ${PORT}`);
-
-  await import('./mqtt/broker.js');
-  await import('./mqtt/mqttClient.js');
-  await import('./mqtt/openskyFetcher.js');
-
-  const { startWorker } = await import('./worker/telemetryProcessor.js');
-  startWorker();
+server.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+  // Worker import will be added later
 });
